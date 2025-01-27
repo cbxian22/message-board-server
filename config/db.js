af9e89553fb421cee89c43797cb1c517cd38e5fd
chenboxian <xian@chenboxiandeMacBook-Air.local>
2025-01-27 10:08:18 +0800

    upadte

--- config/db.js
+++ config/db.js
@@ -1,22 +1,3 @@
-// const mysql = require("mysql2");
-// require("dotenv").config();
-
-// const db = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-// });
-
-// db.connect((err) => {
-//   if (err) {
-//     console.error("資料庫連線錯誤: " + err.stack);
-//     return;
-//   }
-//   console.log("已成功連線到資料庫");
-// });
-
-// module.exports = db;
 const mysql = require("mysql2");
 require("dotenv").config();
 
@@ -25,6 +6,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 db.connect((err) => {
