8d3673be2c04f5e1709b57324df651ccae59d843
chenboxian <xian@chenboxiandeMacBook-Air.local>
2025-02-05 15:22:59 +0800

    c

--- server.js
+++ server.js
@@ -20,21 +20,6 @@ const app = express();
 const server = http.createServer(app); // 使用 http 建立伺服器
 const wss = new WebSocket.Server({ server }); // WebSocket 伺服器與 HTTP 伺服器共用
 
-// === CORS 設定 ===
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://message-board-front.vercel.app"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true"); // 允許攜帶憑證（如 cookies）
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204); // 預檢請求回應 204 No Content
-  }
-  next();
-});
-
 // === WebSocket 連線管理 ===
 wss.on("connection", (ws) => {
   console.log("Client connected");
@@ -48,24 +33,24 @@ wss.on("connection", (ws) => {
   });
 });
 
-app.use(cors());
-// app.use(
-//   cors({
-//     origin: "https://message-board-front.vercel.app", // 只允許前端域名
-//     methods: ["GET", "POST", "PUT", "DELETE"], // 允許的 HTTP 方法
-//     allowedHeaders: ["Content-Type", "Authorization"], // 允許的請求頭，新增 Authorization
-//     credentials: true, // 是否允許攜帶憑證（例如 cookies）
-//   })
-// );
+// app.use(cors());
+app.use(
+  cors({
+    origin: "https://message-board-front.vercel.app", // 只允許前端域名
+    methods: ["GET", "POST", "PUT", "DELETE"], // 允許的 HTTP 方法
+    allowedHeaders: ["Content-Type", "Authorization"], // 允許的請求頭，新增 Authorization
+    credentials: true, // 是否允許攜帶憑證（例如 cookies）
+  })
+);
 
 // WebSocket 廣播函式
-const broadcastMessage = (message) => {
-  wss.clients.forEach((client) => {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(JSON.stringify(message));
-    }
-  });
-};
+// const broadcastMessage = (message) => {
+//   wss.clients.forEach((client) => {
+//     if (client.readyState === WebSocket.OPEN) {
+//       client.send(JSON.stringify(message));
+//     }
+//   });
+// };
 
 // === 中間件設定 ===
 app.use(bodyParser.json());
