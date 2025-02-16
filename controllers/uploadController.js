b32ee4a34d946a5a4f1093f9c4f3013ff1ac9a43
chenboxian <xian@chenboxiandeMacBook-Air.local>
2025-02-16 18:11:28 +0800

    add upload

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,34 +1,3 @@
-// const bucket = require("../config/storage");
-// const { v4: uuidv4 } = require("uuid");
-
-// // 上傳檔案
-// exports.uploadFile = (req, res) => {
-//   const file = req.file;
-
-//   if (!file) {
-//     return res.status(400).json({ error: "請選擇檔案" });
-//   }
-
-//   const fileName = uuidv4() + "." + file.mimetype.split("/")[1]; // 使用 UUID 生成唯一檔名
-//   const fileUpload = bucket.file(fileName);
-
-//   const stream = fileUpload.createWriteStream({
-//     metadata: {
-//       contentType: file.mimetype,
-//     },
-//   });
-
-//   stream.on("finish", () => {
-//     const fileUrl = `https://storage.googleapis.com/${bucket.name}/${fileName}`;
-//     res.status(200).json({ fileUrl });
-//   });
-
-//   stream.on("error", (err) => {
-//     res.status(500).json({ error: "檔案上傳失敗" });
-//   });
-
-//   stream.end(file.buffer);
-// };
 const { Storage } = require("@google-cloud/storage");
 const express = require("express");
 const router = express.Router();
