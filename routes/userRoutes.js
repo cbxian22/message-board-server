17473099c350775db2e1b3c364492cdd8df09934
chenboxian <xian@chenboxiandeMacBook-Air.local>
2025-02-18 19:00:12 +0800

    get usercontroller

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -18,6 +18,6 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
-router.get("/:username", userController.getUserByUsername);
+router.get("/:name", userController.getUserByUsername);
 
 module.exports = router;
