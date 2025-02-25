5ca2210a417b19b7f4836b14f7d28e5a8c9c8144
chenboxian <xian@chenboxiandeMacBook-Air.local>
2025-02-25 09:53:05 +0800

    token

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,17 +1,3 @@
-// const express = require("express");
-// const {
-//   login,
-//   register,
-//   refreshToken,
-// } = require("../controllers/authController");
-// const router = express.Router();
-
-// router.post("/login", login);
-// router.post("/register", register);
-// router.post("/refresh-token", refreshToken);
-
-// module.exports = router;
-
 const express = require("express");
 const {
   login,
@@ -23,8 +9,8 @@ const authMiddleware = require("../middleware/auth");
 const router = express.Router();
 
 router.post("/login", login);
-router.post("/register", register); // 假設你有註冊功能
+router.post("/register", register);
 router.post("/refresh-token", refreshToken);
-router.get("/me", authMiddleware, getCurrentUser); // 獲取當前用戶資訊
+router.get("/me", authMiddleware, getCurrentUser);
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,3 @@
-// const express = require("express");
-// const {
-//   getUserByUsername,
-//   updateUserProfile,
-//   deleteUser,
-// } = require("../controllers/userController");
-
-// const router = express.Router();
-
-// router.get("/:name", getUserByUsername);
-// router.put("/:name", updateUserProfile);
-// router.delete("/:name", deleteUser);
-
-// module.exports = router;
 const express = require("express");
 const {
   getUserByUsername,
