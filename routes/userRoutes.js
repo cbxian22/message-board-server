a96299bd18b392dea9792dccc42d1971d10ea62e
chenboxian <xian@chenboxiandeMacBook-Air.local>
2025-03-08 15:55:32 +0800

    update accountname

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const {
-  getUserByUsername,
+  getUserByAccountname,
   updateUserProfile,
   deleteUser,
 } = require("../controllers/userController");
 const authMiddleware = require("../middleware/auth");
 const router = express.Router();
 
-router.get("/:name", getUserByUsername); // 公開路由，根據名稱獲取用戶資訊
+router.get("/:accountname", getUserByAccountname); // 公開路由，根據名稱獲取用戶資訊
 // router.get("/me", authMiddleware, getCurrentUser);
 router.put("/profile", authMiddleware, updateUserProfile); // 受保護路由，更新用戶資料
 router.delete("/profile", authMiddleware, deleteUser); // 受保護路由，刪除用戶
